845534dd2be13982337fdb537d80fd834c71dc95
Eduardo0515 <183382@ids.upchiapas.edu.mx>
2020-03-20 23:12:49 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {WebSocketsService} from '../app/services/web-sockets.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'proyectcs';
+  constructor (public wsService: WebSocketsService){}
+}
